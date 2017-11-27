cffd734629ddadfbc57ce442b60cc56a00470b8b
anwen01 <2313765710@qq.com>
2017-11-27 15:26:59 +0800

    提交

---
+++ app/src/main/java/com/zyk/lianxi/MainActivity.java
@@ -0,0 +1,17 @@
+package com.zyk.lianxi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zyk.myarr.AddUtil;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int add = AddUtil.add(1, 2);
+        System.out.println("===="+add);
+    }
+}
---
+++ myarr/src/main/java/com/zyk/myarr/AddUtil.java
@@ -0,0 +1,12 @@
+package com.zyk.myarr;
+
+/**
+ * 作者：张玉轲
+ * 时间：2017/11/27
+ */
+
+public class AddUtil {
+    public static int add(int num1,int num2){
+        return num1+num2;
+    }
+}
